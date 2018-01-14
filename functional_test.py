b96d83467adee7c42820b373893c27ad905e7692
Marco Pineda <mapineda1222@gmail.com>
2018-01-14 13:21:24 -0600

    refactor of functional_test.py

--- functional_test.py
+++ functional_test.py
@@ -13,7 +13,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
 
@@ -48,7 +48,7 @@ class NewVisitorTest(unittest.TestCase):
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox = send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
